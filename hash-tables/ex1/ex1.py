58b5e6e9a05f54ad3bb3df446f7f768c85ebdad1
mglittleton <michaelglittleton@gmail.com>
2019-08-07 15:24:58 -0400

    ex1 complete - I think the test #4 is wrong

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -9,15 +9,35 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
-
+    # make sure hash table is big enouogh for all weights, if necessary
+    while length > ht.capacity * 0.7:
+        ht = hash_table_resize(ht)
+    
+    # loop through weights
+    for i in range(len(weights)):
+        # take off current weight from limit
+        balance = limit - weights[i]
+        balance_index = hash_table_retrieve(ht, balance)
+        # if the balance does not exist, add that weight as a future possibility for the next check
+        if balance_index is None:
+            hash_table_insert(ht, weights[i], i)
+        # if there is a pair, return it in the correct format 
+        elif balance > weights[i]:
+            print ("balance",balance, "weight", weights[i])
+            return (balance_index, i)
+        else:
+            print ("balance",balance, "weight", weights[i])
+            return (i, balance_index)
+    
+    # return None if no pair is ever found
     return None
 
 
 def print_answer(answer):
     if answer is not None:
-        print(str(answer[0] + " " + answer[1]))
+        print(str(answer[0]) + " " + str(answer[1]))
     else:
         print("None")
+
+
+print_answer(get_indices_of_item_weights([12, 6, 7, 14, 19, 3, 0, 25, 40], 9, 7))
